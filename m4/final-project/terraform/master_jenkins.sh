629d24363dcddfcbb834ffa9f3772f8a4a626669
nikyta384 <nikyta384@gmail.com>
2022-07-03 16:51:19 +0300

    connect master and slave

---
+++ m4/final-project/terraform/master_jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo yum install docker -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+#mkdir /home/ec2-user/jenkins
+#chmod 777 /home/ec2-user/jenkins
+#wget localhost:8080/jnlpJars/jenkins-cli.jar
